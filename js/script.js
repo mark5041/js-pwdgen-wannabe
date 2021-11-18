4f0d6e691a7e2ae0b681fb1639dfe05f999e6822
mark5041 <mark.beloso5041@gmail.com>
2021-11-18 15:23:11 +0100

    script input and output

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@
 // poi chiedi il suo colore preferito
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
+let username = prompt('inserisci il tuo nome');
+let usersurname = prompt('inserisci il tuo cognome');
+let userfavcolor = prompt('inserisci il tuo colore preferito');
+let welcome = username + ' ' + usersurname + ' ' + userfavcolor;
+document.getElementById('benvenuto').innerHTML = welcome;
