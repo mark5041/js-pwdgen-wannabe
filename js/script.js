5d1cc554c80a93032286c6840fc21e8544d1aa23
mark5041 <mark.beloso5041@gmail.com>
2021-11-18 21:23:47 +0100

    password-update

--- js/script.js
+++ js/script.js
@@ -6,5 +6,7 @@
 let username = prompt('inserisci il tuo nome');
 let usersurname = prompt('inserisci il tuo cognome');
 let userfavcolor = prompt('inserisci il tuo colore preferito');
-let welcome = 'la tua password è:' + username + usersurname + userfavcolor + Math.floor(Math.random() * 999);;
+let favumber = parseInt(prompt('inserisci il tuo numero preferito'));
+favumber = favumber + 5;
+let welcome = 'la tua password è:' + username + usersurname + userfavcolor + favumber + '-' + Math.floor(Math.random() * 999);
 document.getElementById('password').innerHTML = welcome;
